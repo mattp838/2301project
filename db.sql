a974c55f7853c1224e25cbffec1fbe959788be75
pc <petchu@gmail.com>
2011-03-24 18:52:35 -0700

    this is based on v2 of er diagram

--- db.sql
+++ db.sql
@@ -1,15 +1,21 @@
 -- script to create "Video Rental Store" database
--- version 1
 
--- questions: some names you used are reserved words i think, like 'transaction', 'rowid' -Peter
+-- todo items for kristan:
+-- add employee name attribute to employee entity in er diagram
+-- remove employeeID attribute from invoice entity
+-- what is the sales entity for? aren't all the sales recorded in the invoice entity?
+
+-- questions: some names you used are reserved words i think, like 'transaction', category -Peter
+
 
 -- clear tables
+DROP TABLE STOREINFO CASCADE CONSTRAINTS;
 DROP TABLE CUSTOMER CASCADE CONSTRAINTS;
 DROP TABLE EMPLOYEE CASCADE CONSTRAINTS;
 DROP TABLE SALES CASCADE CONSTRAINTS;
 DROP TABLE VIDEOSALES CASCADE CONSTRAINTS;
 DROP TABLE VIDEORENTALS CASCADE CONSTRAINTS;
-DROP TABLE CURRENTLYRENTING CASCADE CONSTRAINTS;
+
 DROP TABLE PROMOS CASCADE CONSTRAINTS;
 DROP TABLE TRANSACTION CASCADE CONSTRAINTS;
 DROP TABLE VIDEO CASCADE CONSTRAINTS;
@@ -17,6 +23,15 @@ DROP TABLE INVOICE CASCADE CONSTRAINTS;
 
 
 -- create the tables
+CREATE TABLE storeInfo
+{
+	name VARCHAR2(30),
+	address VARCHAR2(30),
+	taxNumber NUMBER(20),
+	phone NUMBER(10),
+	CONSTRAINT storeInfo_name_pk PRIMARY KEY (name)
+}
+
 CREATE TABLE customer
 (
 	accountID NUMBER(9),
@@ -33,69 +48,36 @@ CREATE TABLE employee
 (
 	empID NUMBER(6),
 	position VARCHAR2(30),
+	name VARCHAR2(30),
 	CONSTRAINT employee_empID_pk PRIMARY KEY (empID)
 );
 
 
-CREATE TABLE sales
-(
-	rowID NUMBER(6),
-	title VARCHAR2(30),
-	videoID NUMBER(6),
-	dateTime DATE,
-	CONSTRAINT sales_rowID_pk PRIMARY KEY (rowID)
-);
 
 
-CREATE TABLE videoSales
-(
-	saleID NUMBER(6),
-	videoID NUMBER(6),
-	price NUMBER(6),
-	CONSTRAINT videoSales_saleID_pk PRIMARY KEY (saleID)
-);
-
-CREATE TABLE videoRentals
-(
-	rentalID NUMBER(6),
-	price NUMBER(6),
-	status VARCHAR2(10),
-	videoID NUMBER(6),
-	CONSTRAINT videoRentals_rentalID_pk PRIMARY KEY (rentalID)
-);
 
 
-CREATE TABLE currentlyRenting
-(
-	accountID NUMBER(6),
-	videoID NUMBER(6),
-	dateTime DATE,
-	returned VARCHAR(10),
-);
 
 CREATE TABLE promos
 (
 	promoID NUMBER(6),
+	tagname VARCHAR2(30),
 	description VARCHAR2(30),
 	deduction NUMBER(6),
-	tagname VARCHAR2(30),
 	CONSTRAINT promos_promoID_pk PRIMARY KEY (promoID)
 );
 
-CREATE TABLE transaction
+CREATE TABLE invoice
 (
-	rowID NUMBER(6),
-	promoID NUMBER(6),
 	invoiceID NUMBER(6),
-	saleID NUMBER(6),
-	rentalID NUMBER(6),
-	quality VARCHAR2(30),
-	price NUMBER(6)
+	dateTime DATE,
+	
+	CONSTRAINT invoice_invoiceID_pk PRIMARY KEY (invoiceID)
 );
 
-CREATE TABLE video
+CREATE TABLE videoInfo
 (
-	sku NUMBER(11),
+	infoID NUMBER(11),
 	title VARCHAR2(30),
 	genre VARCHAR2(30),
 	synopsis VARCHAR2(300),
@@ -104,7 +86,38 @@ CREATE TABLE video
 	actors VARCHAR2(30),
 	language VARCHAR2(30),
 	director VARCHAR2(30),
-	CONSTRAINT video_sku_pk PRIMARY KEY (sku)
+	rating VARCHAR2(10),
+	-- length of the movie in minutes
+	length NUMBER(11),
+	videoReleaseDate DATE,
+	-- is this needed? theatricalRelaseDate DATE,
+	CONSTRAINT videoInfo_infoID_pk PRIMARY KEY (infoID)
+);
+
+CREATE TABLE physicalVideo
+{
+	SKU VARCHAR2(30),
+	format VARCHAR2(30),
+	condition VARCHAR2(30),
+	-- rentOrSale VARCHAR2(30),
+	CONSTRAINT physicalVideo_SKU_pk PRIMARY KEY (SKU)
+}
+
+CREATE TABLE videoSales
+(
+	saleID NUMBER(6),
+	CONSTRAINT videoSales_saleID_pk PRIMARY KEY (saleID)
+);
+
+CREATE TABLE videoRental
+(
+	rentalID NUMBER(6),
+	checkOutTime DATE,
+	-- maybe we should have a boolean to determine if a video is in or out or just use the two dates to figure it out?
+	-- if checkOutTime > checkInTime, then the video is rented out
+	checkInTime DATE,
+	
+	CONSTRAINT videoRentals_rentalID_pk PRIMARY KEY (rentalID)
 );
 
 CREATE TABLE invoice
